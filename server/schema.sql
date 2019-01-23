54be2d4f9ed1eadb39347f2bd00ad8ff48cfe7c5
Kirill Chernik <kirillchernik8@gmail.com>
2019-01-23 16:58:03 -0500

    created db, connection successful

--- server/schema.sql
+++ server/schema.sql
@@ -1,13 +1,10 @@
 CREATE DATABASE chat;
-
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
-);
-
-/* Create other tables and define schemas for them here! */
-
+CREATE TABLE messages (message_text VARCHAR(200), username INT(200),room INT(20), FOREIGN KEY (username) REFERENCES users(id),FOREIGN KEY (room) REFERENCES rooms (id));
+CREATE TABLE users (id INT(50),messages INT(50)); 
+CREATE TABLE rooms(id INT(50));
+CREATE TABLE messages_users (username INT(50),messages INT(50)); 
 
 
 
